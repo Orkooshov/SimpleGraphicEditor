f6b956435349b0a7b54f28af7bdf1e9215837f10
Orkooshov <60623412+Orkooshov@users.noreply.github.com>
2020-10-13 10:07:44 +0300

    исправление ошибки в названии проекта

--- GraphicalEditor/Properties/Resources.Designer.cs
+++ GraphicalEditor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GraphicalEditor.Properties {
+namespace GraphicEditor.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GraphicalEditor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GraphicalEditor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GraphicEditor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
